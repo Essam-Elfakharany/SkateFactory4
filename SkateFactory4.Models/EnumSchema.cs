21fb173c594fa66f5699c9d14fcb29a7eeab3fe0
Essam-Elfakharany <118538698+Essam-Elfakharany@users.noreply.github.com>
2023-04-04 11:37:20 -0400

    Add project files.

---
+++ SkateFactory4.Models/EnumSchema.cs
@@ -0,0 +1,11 @@
+﻿using SkateFactory4.Models.Enums;
+namespace SkateFactory4.Models;
+
+public class EnumSchema { 
+
+    public EBrakeType BrakeType { get; set; } 
+    public EColor Color { get; set; } 
+    public  EShapeType ShapeType { get; set; } 
+    public ESkateType SkateType { get; set; } 
+    public ESkateboardCriteria SkateboardCriteria { get; set; }
+}
---
+++ SkateFactory4.WebUI/Models/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace SkateFactory4.WebUI.Models;
+
+
+public class UserViewModel
+{
+    [Required]
+    [EmailAddress]
+    [DisplayName("Email")]
+
+    public string ExistingEmail { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+
+    public string Password { get; set; } = string.Empty;
+}
